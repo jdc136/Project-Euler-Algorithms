a0118b1c87faa89d2327d0da72c8a99ee762923c
Jared <49449253+jdcollier136@users.noreply.github.com>
2022-02-16 22:34:15 -0700

    Create P2.cs

---
+++ P2.cs
@@ -0,0 +1,25 @@
+// Problem: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+using System;
+
+namespace P1 {
+    class Program {
+
+        static void Main(string[] args) {
+            int A = 0;
+            int B = 1;
+            int C = 0;
+            int limit = 4_000_000;
+            int sum = 0;
+
+            while (B < limit) {
+                if (B % 2 == 0)
+                    sum = sum + B;
+                //Console.WriteLine("A: {0}, B: {1}, C: {2}", A, B, C);
+                C = A + B;
+                A = B;
+                B = C;
+            }
+            Console.WriteLine("The sum of the even-valued terms whose values do not exceed {0:n0} is {1:n0}.", limit, sum);
+        }
+    }
+}
